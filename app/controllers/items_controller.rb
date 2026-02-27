b84a05aaf243fa6780fbfbf64bd0e77829f590f9
kanoko-west <kanoko.kn2508@gmail.com>
2026-02-27 22:01:02 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,6 @@ class ItemsController < ApplicationController
 
   private 
   def Items_params
-    params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id:current_user.id) 
+    params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price, :item_image).merge(user_id:current_user.id) 
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :orders
+  has_one_attached :image
+
+  validates :item_name, :item_info, :item_category, :item_sales_status, :item_shipping_fee_status, :item_prefecture, :item_scheduled_delivery, :item_price, :item_image, presence:true
 end
