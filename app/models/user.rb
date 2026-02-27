757262ef3495835aaa2436cbd58ad54dc64042e7
kanoko-west <kanoko.kn2508@gmail.com>
2026-02-27 12:24:35 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :last_name,  presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true,format: { with: /\A[ァ-ヶー]+\z/,message: 'は全角（カタカナ）で入力してください' }
+  validates :first_name_kana, presence: true,format: { with: /\A[ァ-ヶー]+\z/,message: 'は全角（カタカナ）で入力してください' }
   validates :birth_date, presence: true
+  validates :password,presence: true,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,message:'は半角英数字混合で入力してください'}
 end
