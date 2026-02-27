d9151fb2d867d1367d16eb06ffd473231f1e4b9f
kanoko-west <kanoko.kn2508@gmail.com>
2026-02-27 15:22:26 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true,format: { with: /\A[ァ-ヶー]+\z/,message: 'は全角（カタカナ）で入力してください' }
   validates :first_name_kana, presence: true,format: { with: /\A[ァ-ヶー]+\z/,message: 'は全角（カタカナ）で入力してください' }
   validates :birth_date, presence: true
-  validates :password,presence: true,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,message:'は半角英数字混合で入力してください'}
+  validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,message:'は半角英数字混合で入力してください'}
 end
