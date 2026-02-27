54b2c48a84503fa9a51fa644286ddf19ec97a45b
kanoko-west <kanoko.kn2508@gmail.com>
2026-02-27 21:49:26 +0900

    newアクション、createアクション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  private 
+  def Items_params
+    params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id:current_user.id) 
+  end
 end
