e917026b79e6ae031b63afca407473c9f17b464d
kanoko-west <kanoko.kn2508@gmail.com>
2026-03-02 14:20:17 +0900

    出品後の動作

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
-  def create
+ def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
 
   private 
-  def Items_params
-    params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price, :item_image).merge(user_id:current_user.id) 
+  def item_params
+    params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :prefecture_id, :item_scheduled_delivery_id, :item_price, :item_image).merge(user_id:current_user.id) 
   end
 end
