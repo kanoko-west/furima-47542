fd9882ec71e595f5c3d6a75958bdb2cfd7ee3661
kanoko-west <kanoko.kn2508@gmail.com>
2026-03-03 21:28:38 +0900

    テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -93,10 +93,15 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Item price must be less than or equal to 9999999")
       end
+      it "item_priceが半角数値でなければ保存できない" do
+        @item.item_price = "abc"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price is not a number")
+      end
       it 'ユーザーが紐付いていなければ投稿できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist')
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
