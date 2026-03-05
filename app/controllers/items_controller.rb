59f1b1e804f65be8ed3ab863ce70c1e635a11147
kanoko-west <kanoko.kn2508@gmail.com>
2026-03-05 21:32:16 +0900

    商品削除機能設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :authorize_owner, only: [:edit, :update]
-  before_action :prevent_edit_if_sold, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_owner, only: [:edit, :update, :destroy]
+  before_action :prevent_edit_if_sold, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
@@ -36,6 +36,12 @@ class ItemsController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
 
   private
 
