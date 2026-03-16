db972a076a8e79429f1f6880d95a6ea0e3e3e6c1
kanoko-west <kanoko.kn2508@gmail.com>
2026-03-16 17:02:35 +0900

    自動デプロイ設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,3 +95,4 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
+server '13.115.18.54', user: 'ec2-user', roles: %w{app db web}
